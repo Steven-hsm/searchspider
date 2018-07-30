b5c5af936fc68e251df6f83e0cdb96ffd5d60465
steven <1586558083@qq.com>
2018-07-30 19:56:01 +0800

    初始化一个spring boot项目

---
+++ src/main/java/com/hsm/searchspider/SearchspiderApplication.java
@@ -0,0 +1,12 @@
+package com.hsm.searchspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchspiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchspiderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hsm/searchspider/SearchspiderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsm.searchspider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchspiderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
